a4d5783d274c27a475b2d851dd8dd2bd89fc107b
Nestor <nestor.retamosa@wethecurious.org>
2019-10-09 13:53:23 +0100

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "EU (London)",
+      BUCKET: "wtc-notes"
+    },
+    apiGateway: {
+      REGION: "https://a90uoqc13m.execute-api.us-east-2.amazonaws.com/prod",
+      URL: "us-east-2"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_nLFYs8pKg",
+      APP_CLIENT_ID: "6n0ld82js40amd9f1ulc1rq39s",
+      IDENTITY_POOL_ID: "us-east-2:f6146900-368d-4240-9dc4-75f2a8134dc0"
+    }
+  };
